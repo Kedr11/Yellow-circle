0e2f93088b357aa5de7b5eba5fc4b74bc7eb281b
Timofey <timohashishkin@gmail.com>
2023-11-23 11:40:27 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QPushButton, QMainWindow
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import uic
+from random import randint
+
+
+class DrawCircle(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadui('untitled.ui', self)
+
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+
+    def draw_circle(self, qp):
+        color = QColor('yellow')
+        x, y = randint(10, 590)
+        d = randint(2, min(x, y, 800 - x, 600 - y) * 2)
+        qp.setBrush(color)
+        qp.drawEllipse(x - d // 2, y - d // 2, d, d)
+
+
+def except_hook(cls, exception, tracebook):
+    sys.__excepthook__(cls, exception, tracebook)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = DrawCircle()
+    ex.show()
+    sys.excepthook = except_hook
+    sys.exit(app.exec())
